19216040bc2832a7898c7abbf537ef95df5f8826
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-02-27 20:23:40 +0100

    update script to copy from wiki

---
+++ update.sh
@@ -0,0 +1,20 @@
+git clone git://github.com/rasmuserik/mLibraries.wiki.git &&
+cd mLibraries.wiki &&
+for md in *.md
+do
+   name=`echo $md | sed -e s/.md$//`
+   echo $name
+   echo --- > "../$md"
+   echo title: $name >> "../$md"
+   echo layout: default >> "../$md"
+   echo --- >> "../$md"
+   cat "$md" | sed -e 's/\[\[\([^]]*\)\]\]/<a href="\1">\1<\/a>/g' >> "../$md"
+   cat "$md" | sed -e 's/\[\[\([^]]*\)\]\]/[\1](http:\/\/mLibraries.org\/\1)/g' | markdown2pdf -o ../$name.pdf
+   cd ..
+   git add $md
+   cd mLibraries.wiki
+done &&
+cd .. &&
+rm -rf mLibraries.wiki &&
+git commit -m "import of pages from wiki" &&
+git push
